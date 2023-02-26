aa82790ba60a49897f0afa9dbb3ff4cd9ddb9182
Muhammed <faheem@metrictreelabs.com>
2023-02-27 00:06:04 +0530

    local db connceted,

--- controllers/getPost.go
+++ controllers/getPost.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"fmt"
+
 	"example.com/m/initializers"
 	"example.com/m/models"
 	"github.com/gin-gonic/gin"
@@ -17,9 +19,17 @@ func GetPosts(c *gin.Context) {
 func GetPostById(c *gin.Context){
 	id := c.Param("id")
 		var data models.Post
-		initializers.DB.First(&data,id)
-		c.JSON(200, gin.H{
+		initializers.DB.Find(&data,id)
+		fmt.Println(data.ID,"hei")
+		if data.ID != 0{
+        c.JSON(200, gin.H{
 			"data":data ,
 		})
+		}else{
+			 c.JSON(200, gin.H{
+			"message":"No items" ,
+		})
+		}
+		
 
 	}
--- initializers/database.go
+++ initializers/database.go
@@ -15,6 +15,8 @@ func ConnectToDB(){
 	dsn := os.Getenv("DB_URL")
 DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
+
+
 if err != nil {
 	 log.Fatal("Failed to connect to database")
 }
